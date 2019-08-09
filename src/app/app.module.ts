fddf5cd4455242bef71f92038c9662889e8aa3f0
gustavosidoti <gustavosidoti@gmail.com>
2019-08-09 07:58:26 -0300

    primer commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,55 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+// Rutas
+import { APP_ROUTES } from './app.routes';
+
+// Modulos
+import { PagesModule } from './pages/pages.module';
+import {SharedModule} from './shared/shared.module';
+
+
+// Componentes principales
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './login/register.component';
+
+
+/*import { HeaderComponent } from './shared/header/header.component';
+import { SidebarComponent } from './shared/sidebar/sidebar.component';     ESTOS SE ENCUENTRAN EN shared.module.ts SEPARADOS
+import { BreadcrumsComponent } from './shared/breadcrums/breadcrums.component';
+import { NopagefoundComponent } from './shared/nopagefound/nopagefound.component';
+*/
+
+/*import { DashboardComponent } from './pages/dashboard/dashboard.component';
+import { ProgressComponent } from './pages/progress/progress.component';
+import { Graficas1Component } from './pages/graficas1/graficas1.component';   ESTOS SE ENCUENTRAN EN pages.module.ts SEPARADOS
+import { PagesComponent } from './pages/pages.component';
+*/
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    RegisterComponent,
+
+   // DashboardComponent,
+    //ProgressComponent, ESTOS SE ENCUENTRAN EN pages.module.ts SEPARADOS
+    //Graficas1Component,
+    // PagesComponent,
+
+   /* HeaderComponent,
+    SidebarComponent,  ESTOS SE ENCUENTRAN EN shared.module.ts SEPARADOS
+    BreadcrumsComponent,
+    NopagefoundComponent,
+   */
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    APP_ROUTES,
+    PagesModule,
+    SharedModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/app.routes.ts
@@ -0,0 +1,25 @@
+import { RouterModule, Routes } from '@angular/router';
+
+import { LoginComponent } from './login/login.component';
+import { PagesComponent } from './pages/pages.component';
+import { RegisterComponent } from './login/register.component';
+import { NopagefoundComponent } from './shared/nopagefound/nopagefound.component';
+
+
+const appRoutes: Routes = [
+  /*{
+    path: '',
+    component: PagesComponent,
+    children: [
+      { path: 'dashboard', component: DashboardComponent },
+      { path: 'progress', component: ProgressComponent },
+      { path: 'graficas1', component: Graficas1Component },
+      { path: '', pathMatch: 'full', redirectTo: 'dashboard' }
+    ]
+  },*/
+  { path: 'login', component: LoginComponent },
+  { path: 'register', component: RegisterComponent },
+  { path: '**', component: NopagefoundComponent }
+];
+
+export const APP_ROUTES = RouterModule.forRoot(appRoutes, {useHash: true} );
